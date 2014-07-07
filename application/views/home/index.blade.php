3d93bce153b9dcb63485013665932f6029519d22
unknown <h-liu@1308104-N.dp.local>
2014-07-07 12:36:00 +0900

    change

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -96,11 +96,9 @@
 <script>
 var app = app || {};
 // Todo Model
-// ----------
-// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+
 app.Todo = Backbone.Model.extend({
-    // Default attributes for the todo
-    // and ensure that each todo created has `title` and `completed` keys.
+ 
     defaults: {
         title: '',
         title_org: '',
@@ -110,7 +108,7 @@ app.Todo = Backbone.Model.extend({
         order: 0,
         completed: "no"
     },
-    // Toggle the `completed` state of this todo item.
+
     toggle: function() {
         this.save({
             completed: ((this.get('completed') == "yes") ? "no" : "yes")
@@ -122,17 +120,13 @@ app.Todo = Backbone.Model.extend({
         }
     }
 });
+
 // Todo Collection
-// ---------------
-// The collection of todos is backed by *localStorage* instead of a remote
-// server.
 var TodoList = Backbone.Collection.extend({
     url: "./api/v1/todos",
     // Reference to this collection's model.
     model: app.Todo,
-    // Save all of the todo items under the `"todos"` namespace.
-    //localStorage: new Store('todos-backbone'),
-    // Filter down the list of all todo items that are finished.
+
     completed: function() {
         return this.filter(function(todo) {
             if (todo.get('completed') == "yes") {
@@ -142,12 +136,7 @@ var TodoList = Backbone.Collection.extend({
             }
         });
     },
-    // Filter down the list to only todo items that are still not finished.
-    remaining: function() {
-        return this.without.apply(this, this.completed());
-    },
-    // We keep the Todos in sequential order, despite being saved by unordered
-    // GUID in the database. This generates the next order number for new items.
+
     nextOrder: function() {
         if (!this.length) {
             return 1;
@@ -159,13 +148,14 @@ var TodoList = Backbone.Collection.extend({
         return todo.get('order');
     }
 });
+
 // Create our global collection of **Todos**.
 app.Todos = new TodoList();
 // Todo Item View
 // --------------
 // The DOM element for a todo item...
 app.TodoView = Backbone.View.extend({
-    //... is a list tag.
+
     tagName: 'li',
     // Cache the template function for a single item.
     template: _.template($('#item-template').html()),
@@ -176,30 +166,24 @@ app.TodoView = Backbone.View.extend({
         'click .toggle': 'togglecompleted'
 
     },
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
-    // app, we set a direct reference on the model for convenience.
+
     initialize: function() {
-        // this.model.on( 'destroy', this.remove, this );
-        //  this.model.on( 'visible', this.toggleVisible, this );
+
     },
-    // Re-render the titles of the todo item.
+
     render: function() {
 
         this.$el.html(this.template(this.model.toJSON()));
         this.$el.toggleClass('completed', (this.model.get('completed') == "yes" ? true : false));
-        // this.toggleVisible();
+
         this.title = this.$('.title');
         this.description = this.$('.description');
 
         this.due_date = this.$('.due_date');
         this.order = this.model.get('order');
         this.title_org = this.model.get('title_org');
-        // this.order=app.Todos.last().get('order');
+
         return this;
-    },
-    toggleVisible: function() {
-        this.$el.toggleClass('hidden', this.isHidden());
     },
 
     ItemControl: function() {
@@ -207,12 +191,6 @@ app.TodoView = Backbone.View.extend({
         $('.item-detail' + order).slideToggle("slow");
     },
 
-    isHidden: function() {
-        var isCompleted = (this.model.get('completed') == "yes") ? true : false;
-        return ( // hidden cases only
-            (!isCompleted && app.TodoFilter === 'completed') || (isCompleted && app.TodoFilter === 'active')
-        );
-    },
 
     // Toggle the `"completed"` state of the model.
     togglecompleted: function() {
@@ -222,14 +200,9 @@ app.TodoView = Backbone.View.extend({
         } else {
             this.$el.find("input[type=checkbox]").attr("checked", false);
         }
-        this.toggleVisible();
         this.model.toggle();
     },
-    // Switch this view into `"editing"` mode, displaying the input field.
-    edit: function() {
-        this.$el.addClass('editing');
-        this.input.focus();
-    },
+
     addOne: function(todo) {
         var view = new app.TodoView({
             model: todo
@@ -271,7 +244,7 @@ app.TodoView = Backbone.View.extend({
         app.Todos.sort();
         this.addAll();
     },
-    // Close the `"editing"` mode, saving changes to the todo.
+
     close: function() {
         var value = this.input.val().trim();
         if (value) {
@@ -284,24 +257,18 @@ app.TodoView = Backbone.View.extend({
         this.$el.removeClass('editing');
     }
 });
-// The Application
-// ---------------
-// Our overall **AppView** is the top-level piece of UI.
+
 app.AppView = Backbone.View.extend({
-    // Instead of generating a new element, bind to the existing skeleton of
-    // the App already present in the HTML.
+  
     el: '#todoapp',
-    // Our template for the line of statistics at the bottom of the app.
+
     statsTemplate: _.template($('#stats-template').html()),
-    // Delegated events for creating new items, and clearing completed ones.
+
     events: {
         'click #addtask_new': 'createOnEnter'
-        // 'click #clear-completed': 'clearCompleted',
-        //  'click #toggle-all': 'toggleAllComplete'
+
     },
-    // At initialization we bind to the relevant events on the `Todos`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved in *localStorage*.
+
     initialize: function() {
         this.input = this.$('#new-todo');
         this.allCheckbox = this.$('#toggle-all')[0];
@@ -315,17 +282,16 @@ app.AppView = Backbone.View.extend({
         app.Todos.fetch();
     },
 
-    // Add a single todo item to the list by creating a view for it, and
-    // appending its element to the `<ul>`.
+
     addOne: function(todo) {
         var view = new app.TodoView({
             model: todo
         });
         $('#sortable-todo').append(view.render().el);
     },
-    // Add all items in the **Todos** collection at once.
+
     addAll: function() {
-        //this.$('#sortable-todo').html('');
+
         $('#sortable-todo').html('');
         var categorised = app.Todos.groupBy(function(todo) {
             return todo.get("month");
@@ -337,7 +303,7 @@ app.AppView = Backbone.View.extend({
         };
     },
   
-    // Generate the attributes for a new Todo item.
+
     newAttributes: function() {
         var due_date = $('.due_date_add').val();
         var month = due_date.substr(5, 2);
@@ -354,8 +320,7 @@ app.AppView = Backbone.View.extend({
             completed: "no"
         };
     },
-    // If you hit return in the main input field, create new **Todo** model,
-    // persisting it to *localStorage*.
+
     createOnEnter: function(e) {
 
         if (!this.input.val().trim()) {
@@ -364,31 +329,13 @@ app.AppView = Backbone.View.extend({
 
         app.Todos.create(this.newAttributes());
         this.input.val('');
-        // var categorised = _.groupBy(app.Todos, function (todo) {
-        //    return todo.month;
-        //  });
 
         this.addAll();
 
     }
 });
-var Workspace = Backbone.Router.extend({
-    routes: {
-        '*filter': 'setFilter'
-    },
-    setFilter: function(param) {
-        // Set the current filter to be used
-        app.TodoFilter = param.trim() || '';
-        // Trigger a collection filter event, causing hiding/unhiding
-        // of Todo view items
-        app.Todos.trigger('filter');
-    }
-});
-app.TodoRouter = new Workspace();
-Backbone.history.start();
-var ENTER_KEY = 13;
+
 $(function() {
-    // Kick things off by creating the **App**.
     new app.AppView();
 }); </script>
     <script>
